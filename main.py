3a30e591d5ec09c5bb5caa49f4800e871f1781cb
Muhammad Saad <160317082+sahersaleem@users.noreply.github.com>
2024-08-10 19:13:13 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# Escape charracters in python
+
+#/n
+print("Hello I am Saher Saleem \nEmerging software engineer.\nhaving skills of html , css and typescript.")
+
+
+# Seperator
+print ("Saher Saleem",18,True, sep="-")
+
+# end
+print ("Saher Saleem",18,True, sep="-", end="\nenter")  
+
+
+
+
+#Multiline comment in python
+"""
+Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.
+"""
+
+
+
+
+#Variables
+
+
+a="saher"
+b=9
+c=9.0
+d=True
+e= None
+
+print("type of a ",type(a))
+print("type of a " ,type(b))
+print("type of a ",type(c))
+print("type of a ",type(d))
+print("type of a ",type(e))
+
+
+
